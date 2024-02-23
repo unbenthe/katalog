d9304ccefd9165a1fc08bb5ce35c0981a03edd7b
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-02-23 14:25:49 -0300

    Create sum_of_positive.py

---
+++ katas/sum_of_positive.py
@@ -0,0 +1,4 @@
+def positive_sum(arr):
+    return sum([item for item in arr if item > 0])
+
+//sum é uma função que soma o que está entre parenteses, ou seja, cada item de uma lista desde que seja positivo (maior q 0)//
