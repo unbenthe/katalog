b3dc612678d53b92eddb33e7c6d512471d9c8a75
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-02-23 15:01:38 -0300

    Create century_from_year.py

---
+++ katas/century_from_year.py
@@ -0,0 +1,8 @@
+def century(year):
+    centuries = year // 100
+    if year % 100 == 0:
+        return centuries
+    else:
+        return centuries + 1
+
+/// double slash gives a integral result of your division, the % 100 guarantees the division left no decimals.///
