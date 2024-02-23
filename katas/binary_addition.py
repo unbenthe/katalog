3a0d72cfa9b2814a82a994b2ee5ac6d3964aa540
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-02-23 15:58:38 -0300

    Update binary_addition.py

--- katas/binary_addition.py
+++ katas/binary_addition.py
@@ -1,4 +1,4 @@
 def add_binary(a,b):
     return bin(a + b).removeprefix('0b')
 
-//o resultado dava 0 e b antes do resultado por algum motivo, descobri que remove prefix existe e dá de tirar assim, mas o resto eu escrevi sozinha :D
+#o resultado dava 0 e b antes do resultado por algum motivo, descobri que remove prefix existe e dá de tirar assim, mas o resto eu escrevi sozinha :D
