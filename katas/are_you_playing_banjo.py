69ae937614c6415070c4ffed4fb32b08b0d5fa53
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-02-27 19:09:01 -0300

    Create are_you_playing_banjo.py

---
+++ katas/are_you_playing_banjo.py
@@ -0,0 +1,7 @@
+def are_you_playing_banjo(name):
+    if name.lower()[0] == 'r':
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+
+# se a primeira letra do nome, forçado a ser minusculo, for 'r', o retorno será "name plays banjo", se não, será "name does not play banjo"
