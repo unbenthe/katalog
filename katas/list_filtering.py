17edf78eea8a10ca4e6a65d5431d4fd13c273f7e
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-02-27 19:39:24 -0300

    Create list_filtering.py

---
+++ katas/list_filtering.py
@@ -0,0 +1,6 @@
+def filter_list(l): 
+    return [character for character in l if type(character) != str]
+
+#depois de sofrer copiei o codigo pra entender melhor e ainda não entendo pq o meu não funcionou
+
+#retorna o caractere da lista se ele não for do tipo "string"
