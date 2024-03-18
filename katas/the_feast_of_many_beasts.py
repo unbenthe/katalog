e325fc5087d8b65250af8929c030db5e764284bb
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-18 16:45:42 -0300

    Create the_feast_of_many_beasts.py

---
+++ katas/the_feast_of_many_beasts.py
@@ -0,0 +1,7 @@
+def feast(beast, dish):
+    if beast [0] != dish [0]:
+        return False 
+    return beast [-1] == dish [-1]    
+
+
+# [-1] é sempre a ultima letra de uma string. != é diferente de.
