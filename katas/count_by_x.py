1adb54b9dd1e1b1dc437f1abb40f5c535c26d26b
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-19 20:17:17 -0300

    Create count_by_x.py

---
+++ katas/count_by_x.py
@@ -0,0 +1,4 @@
+def count_by(x, n):
+    return [i * x for i in range(1, n+1)]
+        
+    # i integral, range = sequencia de numeros que começa no zero e aumenta de um em um (default) e para em numero especificado.
