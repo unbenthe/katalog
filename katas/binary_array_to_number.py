49ec02d529512420d4b6765b1ceb977f9037cc24
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-19 20:44:55 -0300

    Create binary_array_to_number.py

---
+++ katas/binary_array_to_number.py
@@ -0,0 +1,8 @@
+def binary_array_to_number(arr):
+    x = 0
+    for digit in arr:
+        x = x * 2 + digit
+
+    return x
+
+# copiei a resposta porque parei de entender na metade. Guardei pra entender depois
