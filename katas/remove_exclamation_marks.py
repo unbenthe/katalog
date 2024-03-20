1da76428fd6ec74c5f73584e474d9055965353c2
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-19 21:14:03 -0300

    Create remove_exclamation_marks.py

---
+++ katas/remove_exclamation_marks.py
@@ -0,0 +1,10 @@
+def remove_exclamation_marks(s):
+    return s.replace('!', '')
+
+#nem sempre precisa-se usar as funções de mesmo nome para atingir objetivos, replace funcionou muito bem aqui.
+#  Achei essa outra resposta também
+  
+#  def remove_exclamation_marks(s):
+#    s_list = s.split('!')
+#    s = ''.join(s_list)
+#    return s
