a21a6bbeb7915f7d0ce4d3403214ced3136974df
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-22 13:39:22 -0300

    Create return_negative.py

---
+++ katas/return_negative.py
@@ -0,0 +1,9 @@
+def make_negative( number ):
+    if number <=0:
+        return number
+    else:
+        return number * -1
+
+  # Fiz sozinha, é ridiculo mas é um começo kkkkk
+  # "concatenação só funciona em string bla bla bla
+      "
