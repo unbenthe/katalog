198356da20f22f3996acf5bc215a548e42284568
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-22 13:43:03 -0300

    Create build_tower.py

---
+++ katas/build_tower.py
@@ -0,0 +1,11 @@
+def tower_builder(n_floors):
+    result = []
+    width = (n_floors * 2) - 1
+    for x in range(1, 2 * n_floors, 2):
+        stars = x * '*'
+        line = stars.center(width)
+        result.append(line)
+    return result
+
+
+# é uma piramide de asteriscos
