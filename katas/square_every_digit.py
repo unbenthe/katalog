796a157dc8f7e74889bf21de24324e8ce5f7997c
unbenthe <145038842+unbenthe@users.noreply.github.com>
2024-03-27 16:21:54 -0300

    Create square_every_digit.py

---
+++ katas/square_every_digit.py
@@ -0,0 +1,10 @@
+def square_digits(num):
+    square = ""
+    for i in str(num):
+        square += str(int(i) ** 2)
+    return int(square)
+
+#definir variável para guardar resultados
+# por integral na lista:
+# a variável se torna o valor de cada ítem da variante integral que existe na lista vezes ele próprio
+#retornamos a lista de valores integrais
